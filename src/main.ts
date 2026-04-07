bba2f8626841274b8cde6cd89828258ee4532201
zaza090977777@gmail.com <zaza090977777@gmail.com>
2026-04-07 22:40:33 +0200

    updates

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -21,7 +22,7 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  
+
   const port = process.env.PORT || 5000;
   await app.listen(port);
   console.log(`Backend running on port ${port}`);
